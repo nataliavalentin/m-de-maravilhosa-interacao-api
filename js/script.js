934b9783970611f5eab9c3803defc6b9448090f6
analuizasampaio <sampaioaanaluiza@gmail.com>
2019-05-21 22:07:47 -0300

    Consumir API Mulheres negras da história

---
+++ js/script.js
@@ -0,0 +1,39 @@
+let box = document.querySelector('.maravilhosas__box');
+
+fetch('https://theblackwomanhistory.firebaseio.com/.json')
+
+.then((response)=>{
+    return response.json()
+})
+
+.then((data)=>{
+
+    data.content.forEach(mulher => {
+        let perfil = document.createElement('div');
+        perfil.setAttribute('class', 'maravilhosas__perfil');
+        box.appendChild(perfil);
+
+        let link = document.createElement('a');
+        link.href = '#!';
+        perfil.appendChild(link);
+
+        let imagem = document.createElement('img');
+        imagem.setAttribute('class','img-responsive');
+        imagem.src = '#!';
+        link.appendChild(imagem);
+
+        let nome = document.createElement('p');
+        nome.innerHTML = mulher.title;
+        link.appendChild(nome);
+        
+    });
+        
+    });
+
+})
+
+.catch((erro)=>{
+    console.log(erro)
+})
+
+
